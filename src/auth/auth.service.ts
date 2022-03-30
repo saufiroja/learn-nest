72a2632edc30038a15dde3282c9942d0ce12c97a
saufiroja <saufiroja08@gmail.com>
2022-03-30 08:59:20 +0800

    feat: auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { msg: 'hello signup' };
+  async signup(dto: AuthDto) {
+    // generate hash password
+    const hash = await argon.hash(dto.password);
+    // save new user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // select: {
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+      delete user.hash; //(select)
+
+      // return new user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        console.log(error);
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  login() {
-    return { msg: 'hello login' };
+  async login(dto: AuthDto) {
+    // find user by email
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    // if user does not exists throw exception
+    if (!user) {
+      throw new ForbiddenException('Credential incorrect email');
+    }
+
+    // compare password
+    const matches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!matches) {
+      throw new ForbiddenException('Credential incorrect password');
+    }
+
+    delete user.hash;
+
+    return user;
   }
 }
