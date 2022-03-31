da76a3d2cb854287e7c2e074ff4666fe149f9d60
saufiroja <saufiroja08@gmail.com>
2022-03-31 11:34:44 +0800

    clean code

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+
 import { UserController } from './user.controller';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
